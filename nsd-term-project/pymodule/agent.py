c46ed809c27ad89761c2b14ad6e81c96fc62219a
jptom <fmv.asdjk0407.lifebook@gmail.com>
2021-01-11 06:36:17 +0800

    Create agent.py

---
+++ nsd-term-project/pymodule/agent.py
@@ -0,0 +1,21 @@
+
+
+class EstimationAgent(Agent):
+    def __init__(self, time_interval, nu, omega, estimator):
+        super().__init__(nu, omega)
+        self.estimator = estimator
+        self.time_interval = time_interval
+        self.prev_nu = 0.0
+        self.prev_omega = 0.0
+        
+    def decision(self, observation=None):
+        self.estimator.motion_update(self.prev_nu, self.prev_omega, self.time_interval)
+        self.prev_nu, self.prev_omega = self.nu, self.omega
+        self.estimator.observation_update(observation)
+        return self.nu, self.omega
+        
+    def draw(self, ax, elems):
+        self.estimator.draw(ax, elems)
+        x, y, t = self.estimator.pose
+        s = "({:.2f}, {:.2f}, {})".format(x, y, int(t*180/math.pi)%360)
+        elems.append(ax.text(x, y+0.1, s, fontsize=8))
